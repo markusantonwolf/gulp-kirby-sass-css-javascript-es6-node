4d75a0ffae218fc877e819cd5f935cf581cca5ff
Markus Wolf <markus.wolf@craftplaces.com>
2018-08-10 11:44:49 +0200

    clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,23 @@ const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
 const livereload = require('gulp-livereload');
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
 
 const scriptsPath = './application/scripts/**/*.js';
+const stylesPath = './application/styles/**/*.css';
 
 gulp.task('styles', () => {
-  console.info('asdas');
+  console.info('Running gulp styles...');
+  return gulp.src(['./application/styles/reset.css', stylesPath])
+    .pipe(concat('styles.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./public/css/'))
+    .pipe(livereload())
 });
 
 gulp.task('scripts', () => {
-  console.info('running gulp scripts...');
+  console.info('Running gulp scripts...');
   return gulp.src(scriptsPath)
     .pipe(babel({
       presets: ['env']
@@ -24,9 +32,10 @@ gulp.task('default', () => {
   console.info('default');
 });
 
-gulp.task('watch', ()=>{
+gulp.task('watch', () => {
   console.log('Watching you...');
   require('./server.js');
   livereload.listen();
   gulp.watch(scriptsPath, ['scripts']);
+  gulp.watch(stylesPath, ['styles']);
 })
